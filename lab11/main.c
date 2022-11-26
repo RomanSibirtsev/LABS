f01eea740cb4d75f0892212ff2fc750483b839f4
RomanSibirtsev <rom_a04@mail.ru>
2022-11-26 15:16:09 +0000

    lan11

--- lab11/main.c
+++ lab11/main.c
@@ -1,93 +1,114 @@
 #include <stdio.h>
 
+enum State {
+    TRUE,
+    FALSE
+};
+
 int main(void){
 
   char alf[] = "IVXLCDM";
-  int oldest_id = -1, old_id = -1, id = -1, k = 0, old_k = 0, check = 0, check_num = 1, n = 0, res = 0;
+  int oldest_id = -1, old_id = -1, id = -1, k = 1, old_k = 0;
+  int check_num = 1, res = 0, eow = 0, n = 0, slovo = 1;
+  int n_V = 0, n_L = 0, n_D = 0;
+
+  enum State check = FALSE;
+
   for (char ch = getchar(); ch != EOF; ch = getchar())
   {
-    check = 0;
+
+    if ((ch == ' ') || (ch == ',') || (ch == '\t') || (ch == '\n'))
+    {
+      eow = 1;
+    }
+    else
+      eow = 0;
+      
     oldest_id = old_id;
     old_id = id;
+    check = FALSE;
 
     for (int i =  0; i < sizeof(alf)/sizeof(alf[0]); i++)
     {
       if (alf[i] == ch)
       {
-        check = 1;
+        check = TRUE;
         id = i;
       }
     }
-    if ((ch != ' ') && (ch != ',') && (ch != '  ') && (ch != '\n') && (check == 0))
-    {
-      //printf("lol %c ", ch);
-      check_num = 0;
-      id = -1;
-      check = 1;
-    }
-    else if (((ch == ' ') || (ch == ',') || (ch == '  ') || (ch == '\n')) && (n == 0))
-    {
-      check_num = 0;
-      //printf("%d\n", check_num);
-      res += check_num;
-      id = -1;
-      check_num = 1;
-      old_k = 0;
-      continue;
-    }
 
 
-
-    if (check == 1)
+    switch (check)
     {
-      n ++;
-      if ((old_id != -1) && (old_id == id))
-        k++;
-      else
-      {
-        old_k = k;
+    case TRUE:
+        if (ch == 'V')
+        n_V++;
+        if (ch == 'L')
+            n_L++;
+        if (ch == 'D')
+            n_D++;
+
+        n++;
+
+        if (old_id == id)
+            k++;
+        else
+        {
+            old_k = k;
+            k = 1;
+        }
+
+        if (k > 3) // IIII XXXX VVVV
+        {
+            check_num = 0;
+        }
+
+        else if ((k > 1) && ((id == 1) || (id == 3) || (id == 5))) // VV LL DD VVV LLL ...
+        {
+            check_num = 0;
+        }
+
+        else if ((old_k > 1) && (old_id < id)) // IIV VVX IIX ...
+        {
+            check_num = 0;
+        }
+
+        else if ((oldest_id == id) && (oldest_id != -1) && (oldest_id < old_id)) // IXI VXV ...
+        {
+            check_num = 0;
+        }
+
+        else if ((oldest_id < old_id) && (old_id <= id) && (oldest_id != -1) && (old_id != -1)) // CMM IXX
+        {
+            check_num = 0;
+        }
+
+
+        break;
+    
+    case FALSE:
+        if (((ch == ' ') || (ch == ',') || (ch == '\t') || (ch == '\n')) && (eow = 1) && (n > 0) && (slovo == 1))
+        {
+            res += check_num;
+        }
+        slovo = 0;
+        n = 0;
+        check_num = 1;
+        oldest_id = -1;
+        old_id = -1;
+        id = -1;
         k = 1;
-      }
-
-      if ((old_id != -1) && (old_id < id) && (old_k >= 2))
-      {
-        check_num = 0;
-        //printf("a");
-      }
-      if (k > 3)
-      {
-        check_num = 0;
-          //printf("b");
-      }
-      if ((old_id != -1) && (old_id != id) && (oldest_id != -1) && (id == oldest_id) && (id < old_id))
-      {
-        check_num = 0;
-          //printf("c");
-      }
-      if ((old_id != -1) && (old_id != id) && (old_id == 1) && (oldest_id != -1)) // ДОРАБОТАТЬ
-      {
-        check_num = 0;
-          //printf("c");
-      }
-      if ((old_id != -1) && (old_id == id) && (id == 1))
-      {
-        check_num = 0;
-          //printf("d");
-      }
-
-      //printf("%d,%d,%d  ", k, id, old_id);
+        n_V = 0;
+        n_D = 0;
+        n_L = 0;
+        break;
     }
 
-    else
-    {
-      n = 0;
-      //printf("%d\n", check_num);
-      res += check_num;
-      id = -1;
-      check_num = 1;
-      old_k = 0;
-    }
-    //printf("%c",ch);
+    if ((ch == ' ') || (ch == ',') || (ch == '\t') || (ch == '\n'))
+      slovo = 1;
   }
-  printf("%d\n", res);
+
+  printf("res = %d\n", res);
+  return 0;
+
 }
